5eb50fee7e3b914fbb9226bd63edbc80971a0ad8
Marko Milosevic <pretor@linux-cxer.site>
2015-11-02 20:56:22 +0100

    missing USE fix

--- Controller/ContactController.php
+++ Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace KPhoen\ContactBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class ContactController extends BaseContactController
